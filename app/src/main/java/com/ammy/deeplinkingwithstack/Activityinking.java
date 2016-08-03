96947d5dd28bf34a19739a80e3e2d0d75a9a8479
amandeepsingh <aman.deep@evontech.com>
2016-08-03 14:53:28 +0530

    Initial commit

---
+++ app/src/main/java/com/ammy/deeplinkingwithstack/Activityinking.java
@@ -0,0 +1,13 @@
+package com.ammy.deeplinkingwithstack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activityinking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activityinking);
+    }
+}
